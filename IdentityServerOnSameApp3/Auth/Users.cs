9ff55f499c826f15a34ffc087f35e0e4b44592b9
Andy Li <andy2008@gmail.com>
2016-02-05 13:57:08 +1030

    Sample project

---
+++ IdentityServerOnSameApp3/Auth/Users.cs
@@ -0,0 +1,106 @@
+﻿using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services.InMemory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IdentityServerOnSameApp3.Auth
+{
+    static class Scopes
+    {
+        public static List<Scope> Get()
+        {
+            return new List<Scope>
+            {
+                new Scope
+                {
+                    Name = "api1"
+                },
+                new Scope
+                {
+                    Name = "read",
+                    DisplayName = "Read data"
+                },
+            };
+        }
+    }
+
+    static class Users
+    {
+        public static List<InMemoryUser> Get()
+        {
+            return new List<InMemoryUser>
+            {
+                new InMemoryUser
+                {
+                    Username = "bob",
+                    Password = "bob",
+                    Subject = "1"
+                },
+                new InMemoryUser
+                {
+                    Username = "alice",
+                    Password = "alice",
+                    Subject = "2"
+                }
+            };
+        }
+    }
+
+    static class Clients
+    {
+        public static List<Client> Get()
+        {
+            return new List<Client>
+            {
+                // no human involved
+                new Client
+                {
+                    ClientName = "Silicon-only Client",
+                    ClientId = "silicon",
+                    Enabled = true,
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    Flow = Flows.ClientCredentials,
+
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = new List<string>
+                    {
+                        "api1"
+                    }
+                },
+
+                // human is involved
+                new Client
+                {
+                    ClientName = "Silicon on behalf of Carbon Client",
+                    ClientId = "carbon",
+                    Enabled = true,
+                    AccessTokenType = AccessTokenType.Reference,
+
+                    Flow = Flows.AuthorizationCode,
+
+                    ClientSecrets = new List<Secret>
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    AllowedScopes = new List<string>
+                    {
+                        "api1", "read"
+                    },
+
+                    RedirectUris = new List<string>
+                    {
+                        "https://www.getpostman.com/oauth2/callback"
+                    }
+                }
+            };
+        }
+    }
+}
---
+++ IdentityServerOnSameApp3/Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+using IdentityServer3.AccessTokenValidation;
+using IdentityServer3.Core.Configuration;
+using IdentityServerOnSameApp3.Auth;
+using Serilog;
+
+[assembly: OwinStartup(typeof(IdentityServerOnSameApp3.Startup))]
+
+namespace IdentityServerOnSameApp3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map("/identity", inner =>
+            {
+                var option = new IdentityServerOptions
+                {
+                    RequireSsl = false,
+                    SiteName = "Same Server Identity Service",
+                    EnableWelcomePage = false,
+                    Factory = new IdentityServerServiceFactory()
+                            .UseInMemoryClients(Clients.Get())
+                            .UseInMemoryScopes(Scopes.Get())
+                            .UseInMemoryUsers(Users.Get())
+                };
+
+                inner.UseIdentityServer(option);
+            });
+
+            app.UseIdentityServerBearerTokenAuthentication(new IdentityServerBearerTokenAuthenticationOptions()
+            {
+                Authority = "http://localhost:4560/identity",
+                ValidationMode = ValidationMode.Both,
+                RequiredScopes = new[] { "read", "api1" },
+                DelayLoadMetadata = true
+            });
+
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug() // change with your desired log level
+                .WriteTo.Trace()
+                //.WriteTo.File(@"C:\myPath.txt") // remember to assign proper writing privileges on the file
+                .CreateLogger();
+
+        }
+    }
+}
